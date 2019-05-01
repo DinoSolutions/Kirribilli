6943ecd67b14101d54b174183e25dba13a3b410d
Dino Wang <anguste@gmail.com>
2019-05-02 09:33:25 +1000

    Added data_prep function to include all non-zero functions

--- parse.py
+++ parse.py
@@ -1,30 +1,33 @@
 # import pandas
 import numpy
 import json
-from asammdf import MDF, Signal
-from prototype import (
-    TEST_FILES,
-    SLOW_SIGNALS,
-    SIGNALS,
-)
 from collections import namedtuple
+
+from asammdf import MDF, Signal
+
+from prototype import TEST_FILES
 
 
 def read_config(config):
-    with open(config) as config_file:
-        cfg = json.load(config_file)
+    try:
+        with open(config) as config_file:
+            cfg = json.load(config_file)
+            print('Config file [%s] loaded.' % config)
+    except FileNotFoundError:
+        print('Config file [%s] does not exist' % config)
+        return
     return cfg
 
 
 def file_selector():
     filename = TEST_FILES[0]
-    print('\nInput Filename: %s' % filename)
+    print('Input Filename: %s' % filename)
     return filename
 
 
 def file_version(filename):
     with MDF(filename) as mdf_file:
-        print('\nMDF Version: %s' % mdf_file.version)
+        # print('\nMDF Version: %s' % mdf_file.version)
         return mdf_file.version
 
 
@@ -32,11 +35,10 @@ def list_all_signals(filename):
     with MDF(filename) as mdf_file:
         counter = 0
         for channel in mdf_file.iter_channels():
-            # print(channel.name + '\t[' + channel.unit + ']' + '\t' + channel.source[1])
             print(str(counter) + '\t' + channel.name)
             counter += 1
         print('\nTotal Number of Signals: ' + str(counter) + '\n')
-    return
+    return counter
 
 
 def get_signal_index(signalname, filename):
@@ -95,11 +97,111 @@ def data_prep(filename, cfg, freq=0.01):
         #         # print(counter, channel.name, signals_normal[counter])
         #         channel.name = signals_normal[counter]
         #         counter += 1
-        signals = mdf_output.select(signals_raw, raw=False, dataframe=False)
+        signals = mdf_output.select(signals_raw, raw=True, dataframe=False, record_offset=0, copy_master=True)
+        for i, sig in enumerate(signals):
+            # print(sig.timestamps[0], '\t', sig.timestamps[numpy.size(sig.samples)-1], '\t', numpy.size(sig.samples))
+            if sig.conversion and sig.conversion.conversion_type < 7:
+                signals[i] = sig.physical()
         sql_dtype = data_type(signals)
         # Generate timestamps
         signal_t = signals[0].timestamps
         return signals, sql_dtype, signals_normal, signals_raw, signal_t
+
+
+def data_prep_full(filename, sample_rate=0.01):
+
+    with MDF(filename) as mdf_file:
+
+        # Construct MDF object: exclude empty channels and channels with attachment
+        selected_signals = list()
+        for n, channel in enumerate(mdf_file.iter_channels()):
+            if numpy.size(channel.samples) == 0:
+                continue
+            # elif channel.attachment:
+            #     continue
+            else:
+                selected_signals.append(channel.name)
+        mdf_filter = mdf_file.filter(selected_signals)
+
+        mdf_resample = mdf_filter.resample(raster=sample_rate)
+
+        # Determine latest first timestamp
+        t_start = list()
+        t_end = list()
+        n_sample = list()
+        for n, channel in enumerate(mdf_resample.iter_channels()):
+            t_start.append(channel.timestamps[0])
+            t_end.append(channel.timestamps[-1])
+            n_sample.append(numpy.size(channel.samples))
+        t1_max, t1_min = numpy.amax(t_start), numpy.amin(t_start)
+        te_max, te_min = numpy.amax(t_end), numpy.amin(t_end)
+        s = '{:{f}}\t{:{f}}\t{:{f}}\t{:{f}}'
+        print(s.format(t1_max, t1_min, te_max, te_min, f='.4f'))
+        print(numpy.amax(n_sample), numpy.amin(n_sample))
+        print(len(set(n_sample)), ':\t', set(n_sample))
+
+        # Construct MDF object: starting from the latest first timestamp
+        mdf_align = mdf_filter.cut(start=t1_max, stop=te_min, whence=0, include_ends=True)
+
+        # Veryfication of aligned MDF object
+        t_start = list()
+        t_end = list()
+        n_sample = list()
+        for n, channel in enumerate(mdf_align.iter_channels()):
+            t_start.append(channel.timestamps[0])
+            t_end.append(channel.timestamps[-1])
+            n_sample.append(numpy.size(channel.samples))
+        t1_max, t1_min = numpy.amax(t_start), numpy.amin(t_start)
+        te_max, te_min = numpy.amax(t_end), numpy.amin(t_end)
+        s = '{:{f}}\t{:{f}}\t{:{f}}\t{:{f}}'
+        print(s.format(t1_max, t1_min, te_max, te_min, f='.4f'))
+        print(numpy.amax(n_sample), numpy.amin(n_sample))
+        print(len(set(n_sample)), ':\t', set(n_sample))
+    return
+
+
+def data_prep_full_proto(filename):
+    with MDF(filename) as mdf_file:
+        # Number of all channels in MDF
+        n_channels = sum(len(group.channels) for group in mdf_file.groups)
+
+        # Getting numbers of samples for each channel
+        counter = 0
+        n_samples = numpy.empty(n_channels, numpy.int)
+        i_channels = list()
+        for i, group in enumerate(mdf_file.groups):
+            for j, channel in enumerate(group.channels):
+                n_samples[counter] = group.channel_group.cycles_nr
+                i_channels.append((channel.name, i, j))
+                counter += 1
+        print(n_samples, len(n_samples), len(set(n_samples)))
+        # Non-zero minimum amount of samples
+        # indices = numpy.where(n_samples == numpy.min(n_samples[numpy.nonzero(n_samples)]))
+        # Looking for channels with maximum number of samples
+        indices = numpy.where(n_samples == numpy.max(n_samples))
+        # indices[0][0] is timestamp, indices[0][1] is actual channel
+        # signal_index in format: (channel name, group index, channel index) tuple
+        signal_index = i_channels[indices[0][1]]
+        # Select one channel that has the most samples hence longest timestamps
+        long_signal = mdf_file.select([signal_index])
+
+        # Generating resampled MDF
+        # Among 3 given options for raster, 2 will fail
+        raster_array = long_signal[0].timestamps  # this will fail
+        raster_name = signal_index[0]  # this will fail too
+        raster_float = 0.01  # only this works
+        mdf_file.configure(integer_interpolation=0)
+        mdf_resample = mdf_file.resample(raster_array)
+
+        # Verification
+        counter = 0
+        n_samples = numpy.empty(n_channels, numpy.int)
+        for group in mdf_resample.groups:
+            for channel in group.channels:
+                n_samples[counter] = group.channel_group.cycles_nr
+                counter += 1
+        print(n_samples, len(n_samples), len(set(n_samples)))
+    return
 
 
 def data_type(signals):
@@ -124,16 +226,21 @@ def map_init(lat_s, lat_n, lng_w, lng_e, bound_factor):
     clng = (lng_e + lng_w) / 2
     dlat = math.radians(lat_n - lat_s)
     dlng = math.radians(lng_e - lng_w)
-    # Haversine formula: calculate the great-circle distance between two points
+
+    # Haversine formula:
+    # Calculate the great-circle distance between two points
     a = math.sin(dlat / 2) ** 2 + math.cos(lat_s) * math.cos(lat_n) * math.sin(dlng / 2) ** 2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
     # Mean radius of earth in km: 6371
     great_circle_distance = c * 6371
+
     # Minimum map tile size 256 x 256 pixels
     zoom = math.floor(8 - math.log(bound_factor * great_circle_distance / math.sqrt(2 * 256 * 256))) / math.log(2)
+
     # Convert zoom level from float to integer
     zoom = int(zoom)
-    print(great_circle_distance, clat, clng, zoom)
+    # print(great_circle_distance, clat, clng, zoom)
     return clat, clng, zoom
 
 
@@ -141,8 +248,10 @@ def main():
     filename = file_selector()
     file_version(filename)
     # list_all_signals(filename)
-    cfg = read_config('config_signals.json')
-    signals, dtype, norm, raw, t = data_prep(filename, cfg, freq=0.01)
+    # cfg = read_config('config_signals.json')
+    # signals, dtype, norm, raw, t = data_prep(filename, cfg, freq=0.01)
+    data_prep_full(filename)
+    # data_prep_full_proto(filename)
     return
 
 
